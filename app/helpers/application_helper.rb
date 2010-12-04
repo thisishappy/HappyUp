4bcae9a2beac4909656c8d0e54925bcab5cbeb1d
ricburton <richard@thisishappy.com>
2010-12-04 20:15:33 +0000

    some title changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return a title whether the @instance_variable is set or not.
+  def title
+    base_title = "Happy&reg; &middot; A new kind of company."
+    if @title.nil?
+      base_title
+    else
+      "#{@title} &middot; #{base_title}"
+    end
+  end
 end
